08ae236303b405fb795a3659c62b821fca22c1e3
FyFean <72227868+FyFean@users.noreply.github.com>
2020-11-26 22:59:38 +0100

    chat handlebars
    ustvarjeni chat handlebarsi

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -29,7 +29,71 @@ const viewProfile = (req, res) => {
 
 /* GET chat page */
 const chat = (req, res) => {
-  res.render('chat', { title: "Sporočila" });
+  res.render('chat', { 
+    title: "Sporočila",
+    contacts: [{
+      ime: "Steve Gates",
+      slika: "/assets/pics/instruktor1.jpg"
+    },
+    {
+      ime: "Billa Jobs",
+      slika: "/assets/pics/instruktor3.jpg"
+    },
+    {
+      ime: "Mark Zuki",
+      slika: "/assets/pics/instruktor1.jpg"
+    }
+    ],
+
+    contactChosen:{ 
+      sporocila: [
+        {
+          ime: "Billa Jobs",
+          slika: "/assets/pics/instruktor3.jpg",
+          besedilo: "besedilo sporocila 1",
+          cas: "16:20 | Nov 15"
+        },
+        {
+          ime: "Billa Jobs",
+          slika: "/assets/pics/instruktor3.jpg",
+          besedilo: "besedilo sporocila 2",
+          cas: "16:23 | Nov 15"
+        },
+        {
+          ime: "Billa Jobs",
+          slika: "/assets/pics/instruktor3.jpg",
+          besedilo: "besedilo sporocila 3",
+          cas: "16:26 | Nov 15"
+        }
+      ],
+      datoteke: [
+        {imeDatoteke: "program.c"},
+        {imeDatoteke: "slika.jpg"},
+        {imeDatoteke: "seminarska.ppt"},
+      ]
+    },
+    mojaSporocila: [
+      {
+        ime: "Rozi Furst",
+        slika: "/assets/pics/instruktor2.jpg",
+        besedilo: "to sem poslal jaz 1",
+        cas: "16:21 | Nov 15"
+      },
+      {
+        ime: "Rozi Furst",
+        slika: "/assets/pics/instruktor2.jpg",
+        besedilo: "to sem poslal jaz 2",
+        cas: "16:24 | Nov 15"
+      },
+      {
+        ime: "Rozi Furst",
+        slika: "/assets/pics/instruktor2.jpg",
+        besedilo: "to sem poslal jaz 3",
+        cas: "16:26 | Nov 15"
+      }
+    ]
+
+  });
 };
 
 module.exports = {
