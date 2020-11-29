6512b480358c443724358181f83a76d716bc780b
Sara Aleksieska <sa9808@student.uni-lj.si>
2020-11-29 18:09:38 +0100

    spremenjena lokacija profila, izbrisan iz te lokacije

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -3,46 +3,6 @@ const dashboard = (req, res) => {
   res.render('dashboard', { title: "Nadzorna plošča" });
 };
 
-/* GET profile for viewers */
-const viewProfile = (req, res) => {
-  res.render('profile-view', {
-    title: 'Profil',
-    glavniPodatki: {
-      ime: 'Janez',
-      priimek: "Novak",
-      email: "janez.novak@gmail.com",
-      geslo: "password123",
-      statusInstruktorja: false
-    },
-    datumVpisa: {
-      $date: "2019-11-07T00:00:00Z"
-    },
-    dodatniPodatki: {
-      slika: '/assets/pics/instruktor1.jpg',
-      opis: "Sem Janez in obožujem številke!",
-      telefonskaStevilka: "041 532 234",
-      ocena: 4
-    },
-    dela: [{
-      stevilka: '1',
-      ime: 'Linearna Algebra',
-      $date: '2020-10-07T00:00:00Z'
-    },{
-      stevilka: '2',
-      ime: 'Osnovna Matematična Analiza',
-      $date: '2020-11-07T00:00:00Z'
-    }],
-    instrukcije: [{
-      stevilka: '1',
-      ime: 'Programiranje 1',
-      $date: '2020-11-07T00:00:00Z'
-    }, {
-      stevilka: '2',
-      ime: 'Programiranje 2',
-      $date: '2020-11-07T00:00:00Z'
-    }]
-  });
-};
 
 /* GET chat page */
 const chat = (req, res) => {
@@ -51,6 +11,5 @@ const chat = (req, res) => {
 
 module.exports = {
   dashboard,
-  viewProfile,
   chat
 };
