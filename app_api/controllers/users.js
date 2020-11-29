46b8c1a1eb0cf16e836933c68e5c67ad86071c41
Sara Aleksieska <sa9808@student.uni-lj.si>
2020-11-29 18:13:23 +0100

    spremenjena shema za uporabnika

---
+++ app_api/controllers/users.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const instrukcijeDodatekShema =require('./instructions');
+const deloShema = require('./jobs');
+/*
+const uporabnikDodatnoShema = new mongoose.Schema({
+  opis: String,
+  telefonskaStevilka: [Number],
+  ocena: { type: Number, "default": 0, min: 0, max: 5 }
+});
+*/
+const uporabnikZacetnoShema = new mongoose.Schema({
+  ime: { type: String, required: true },
+  priimek: { type: String, required: true },
+  opis: {type: String},
+  telefonskaStevilka: {type: String},
+  ocena: {type: Number},
+  email: { type: String, required: true },
+  geslo: { type: String, required: true },
+  statusInstruktorja: { type: Boolean, "default": false },
+  datumVpisa: { type: Date, "default": Date.now },
+  dogodki: [instrukcijeDodatekShema],
+  dela: [deloShema]
+});
+
+mongoose.model('User', uporabnikZacetnoShema, 'Users');
