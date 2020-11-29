5568aa4d9bff964fba6862263c3ce42f34ffe98e
Sara Aleksieska <sa9808@student.uni-lj.si>
2020-11-30 00:40:55 +0100

    MODEL ZA USERA UPORABEN

--- app_api/models/users.js
+++ app_api/models/users.js
@@ -1,68 +1,26 @@
 const mongoose = require('mongoose');
-const uporabnikObveznostShema = new mongoose.Schema({
-  stevilka: {type: Number},
-  dogodek: {type: String},
-  datum: {type: String}
+const instrukcijeDodatekShema =require('./instructions');
+const deloShema = require('./jobs');
+/*
+const uporabnikDodatnoShema = new mongoose.Schema({
+  opis: String,
+  telefonskaStevilka: [Number],
+  ocena: { type: Number, "default": 0, min: 0, max: 5 }
 });
-
-const uporabnikViewShema = new mongoose.Schema({
-  delo: [uporabnikObveznostShema],
-  instrukcija: [uporabnikObveznostShema]
-});
-
-const uporabnikUserObvestiloShema = new mongoose.Schema({
-  stevilka: {type: Number},
-  dogodek: {type: String},
-  datum: {type: Date, "default": Date.now}, //datum spremembe ali?
-  povezava: {type: String} 
-});
-
-const uporabnikUserShema = new mongoose.Schema({
-  objava: [uporabnikObveznostShema],
-  obvestila: [uporabnikUserObvestiloShema]
-});
-
-const uporabnikShema = new mongoose.Schema({
+*/
+const uporabnikZacetnoShema = new mongoose.Schema({
   ime: { type: String, required: true },
   priimek: { type: String, required: true },
+  opis: {type: String, "default": ""},
+  telefonskaStevilka: {type: String, "default": ""},
+  ocena: {type: Number, "default": 5},
   email: { type: String, required: true },
   geslo: { type: String, required: true },
   statusInstruktorja: { type: Boolean, "default": false },
   datumVpisa: { type: Date, "default": Date.now },
-  opis: String,
-  slika: String, 
-  telefonskaStevilka: [Number],
-  ocena: { type: Number, "default": 0, min: 0, max: 5 },
-  mojProfil: {type: uporabnikUserShema},
-  viewProfil: {type: uporabnikViewShema}
+  dogodki: [instrukcijeDodatekShema],
+  dela: [deloShema]
 });
 
-mongoose.model('User', uporabnikShema, 'Users');
 
-/*
-db.Users.save({ 
-    ime: 'Petfddsra',
-    priimek: 'Kudsfralt',
-    email: 'pkfd@gmail.com',
-    geslo: 'ljubimsfdfaro888',
-    statusInstruktorja: false,
-    opis: 'ponavadi procrastinatam',
-    telefonskaStevilka: 89767,
-    ocena: 5,
-  
-    mojProfil: {
-      objava: {
-      stevilka: 1,
-        dogodek: 'sp party',
-        datum: '27.11', 
-      },
-      obvestila: {
-      stevilka: 1,
-        dogodek: 'imas sporocilo od myonelovesara',
-        povezava: '#', 
-      
-      }
-  }
-  });
- */
-  
+mongoose.model('User', uporabnikZacetnoShema, 'Users');
