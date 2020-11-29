4ed66f29a592d52edbc1e783f42662c1ad416cbb
Sara Aleksieska <sa9808@student.uni-lj.si>
2020-11-29 17:52:42 +0100

    Dodan helper za stetje elementov seznama

--- app_server/views/helpers/hbsh.js
+++ app_server/views/helpers/hbsh.js
@@ -13,3 +13,24 @@ hbs.registerHelper('zvezdice', (ocena) => {
     zvezdice += '<i class="fa' + (ocena >= i ? 's' : 'r') + ' fa-star"></i>';
   return zvezdice;
 });
+
+hbs.registerHelper('ujemanjeNiza', (vrednost1, vrednost2, moznosti) => {
+  return (vrednost1 == vrednost2) ? moznosti.fn(this) : moznosti.inverse(this);
+});
+
+
+hbs.registerHelper("inc", function(value, options)
+{
+    return parseInt(value) + 1;
+});
+
+
+/*
+hbs.registerHelper('posodobiProfil', (data) => {
+  editProfile(data)
+})
+
+hbs.registerHelper('dateFormat', function (date, options) {
+  const formatToUse = (arguments[1] && arguments[1].hash && arguments[1].hash.format) || "DD/MM/YYYY"
+  return moment(date).format(formatToUse);
+});*/
