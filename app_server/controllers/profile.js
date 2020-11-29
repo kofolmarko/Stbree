9e0343cce4a81bf461a2875f09aec21b398b4968
Sara Aleksieska <sa9808@student.uni-lj.si>
2020-11-29 18:07:23 +0100

    Spremenjen oz. dodan controller za profila (prikaz podatkov UI)

--- app_server/controllers/profile.js
+++ app_server/controllers/profile.js
@@ -1,97 +1,73 @@
-/*const axios = require('axios');
+//API connection
+const axios = require('axios');
+
+//API local parameters
 var apiParametri = {
   streznik: 'http://localhost:' + (process.env.PORT || 3000)
 };
 
-if(process.env.NODE_ENV == 'production') {
-  apiParametri.streznik = 'https://stbree.herokuapp.com'
-}*/
+//API Mogno Atlas parameters
+if (process.env.NODE_ENV === 'production') {
+  apiParametri.streznik = 'https://stbree.herokuapp.com';
+}
 
-/*GET user profile page*/
-const userProfile = (req, res) => {
-  res.render('profile-user', {
-      title: 'Moj profil',
-      glavniPodatki: {
-          ime: 'Lara',
-          priimek: 'Jontes',
-          email: 'lara.jontes@gmail.com',
-          geslo: 'lara1234',
-          statusInstruktorja: true
-      },
-      datumVpisa: {
-          $date: '2020-11-07T00:00:00Z'
-      },
-      dodatniPodatki: {
-          slika: '/assets/pics/profile_picture.png',
-          opis: 'Sem študentka drugega letnika FEja in rada programiram!',
-          telefonskaStevilka: '041 442 224',
-          ocena: 5
-      },
-      objave:[{
-          stevilo: '1',
-          projekt: 'Logo za projekt SP',
-          $date: '2020-01-07T00:00:00Z',
-      },{
-          stevilo: '2',
-          projekt: 'Projekt SP',
-          $date: '2020-02-07T00:00:00Z',
-      }],
-      obvestila: [{
-          stevilo: '1',
-          nenavadno: 'Anže Vtičnik je spremenil datum inštrukcije - Programiranje 1: rekurzija',
-          povezava: '/ponudba-del/delo',
-          $date: '2020-11-07T00:00:00Z'
-      }, {
-          stevilo: '2',
-          nenavadno: 'Dobil si novo sporočilo!',
-          povezava: '/sporocanje',
-          $date: '2020-10-07T00:00:00Z'
-      }]
-  });
-};
-
-/* GET profile for viewers */
-const viewProfile = (req, res) => {
-    res.render('profile-view', {
-      title: 'Profil',
-      glavniPodatki: {
-        ime: 'Janez',
-        priimek: "Novak",
-        email: "janez.novak@gmail.com",
-        geslo: "password123",
-        statusInstruktorja: false
-      },
-      datumVpisa: {
-        $date: "2019-11-07T00:00:00Z"
-      },
-      dodatniPodatki: {
-        slika: '/assets/pics/instruktor1.jpg',
-        opis: "Sem Janez in obožujem številke!",
-        telefonskaStevilka: "041 532 234",
-        ocena: 4
-      },
-      dela: [{
-        stevilka: '1',
-        ime: 'Linearna Algebra',
-        $date: '2020-10-07T00:00:00Z'
-      },{
-        stevilka: '2',
-        ime: 'Osnovna Matematična Analiza',
-        $date: '2020-11-07T00:00:00Z'
-      }],
-      instrukcije: [{
-        stevilka: '1',
-        ime: 'Programiranje 1',
-        $date: '2020-11-07T00:00:00Z'
-      }, {
-        stevilka: '2',
-        ime: 'Programiranje 2',
-        $date: '2020-11-07T00:00:00Z'
-      }]
+const loginID = require('./signing').loginID;
+const renderProfile = (req, res) => {
+    axios.get("http://localhost:3000/api/uporabnik/" + req.params.idUporabnika)
+    .then(({data}, err) => {
+      console.log(data);
+      const profileDetails = {
+        ime: data.ime,
+        priimek: data.priimek,
+        opis: data.opis,
+        telefonskaStevilka: data.telefonskaStevilka,
+        ocena: data.ocena,
+        datumVpisa: data.datumVpisa,
+        email: data.email,
+        statusInstruktorja: data.statusInstruktorja,
+        dogodki: data.dogodki,
+        dela: data.dela,
+        isAdmin: req.params.idUporabnika === loginID.val
+      }
+      let sporocilo = "";
+      console.log("Data :" + data)
+      res.render('profile', {
+          profileDetails: profileDetails, 
+          sporocilo: sporocilo});
+    })
+    .catch((err) => {
+        let sporocilo = "User not good";
+        res.render('profile',{
+            profileDetails: {
+               
+            },
+            sporocilo: sporocilo
+        })
     });
-};
+  };
 
-module.exports = {
-    userProfile,
-    viewProfile
-};
+  /*
+  function editProfile(data) {
+      axios.put("http://localhost:3000/api/uporabnik/posodobi/" + loginID, {
+        email: data.email,
+        telefonskaStevilka: data.telefonskaStevilka,
+        statusInstruktorja: data.statusInstruktorja,
+        datumVpisa: data.datumVpisa,
+        opis: data.opis,
+        geslo: data.geslo
+      })
+      .then(({data}, err) => {
+        document.location.reload();
+        let sporocilo = "";
+        console.log("Data :" + data)
+      })
+      .catch((err) => {
+          let sporocilo = "User not good";
+      });
+    };*/
+
+  module.exports = {
+    renderProfile,
+    //editProfile
+  };
+  
