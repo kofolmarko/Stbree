b9f1c4c8e29449149ac084cb390710bf127edd2e
Marko Kofol <mk8394@student.uni-lj.si>
2020-12-23 17:56:09 +0100

    Produkcijska verzija Angular za Heroku deployment.

---
+++ app_public/build/assets/javascripts/div-scroll.js
@@ -0,0 +1,15 @@
+var files = document.getElementById("file-list");
+files.scrollTop = files.scrollHeight;
+
+function updateScroll(){
+    var files = document.getElementById("file-list");
+    files.scrollTop = files.scrollHeight;
+}
+
+var messages = document.getElementById("messages");
+messages.scrollTop = messages.scrollHeight;
+
+function updateScroll(){
+    var messages = document.getElementById("messages");
+    messages.scrollTop = messages.scrollHeight;
+}
