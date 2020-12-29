a8199a44dc041ada2a6020995646869c38026477
Gregor Devetak <gd2458@student.uni-lj.si>
2020-12-30 00:33:19 +0100

    delo na komponentah komentarjev

--- app_public/src/app/common/components/komentarji/komentarji-seznam/komentarji-seznam.component.ts
+++ app_public/src/app/common/components/komentarji/komentarji-seznam/komentarji-seznam.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+import { InstructionsService } from '../../../services/instructions.service';
+
+import { InstructionsEvent } from '../../../classes/event';
+import { User } from '../../../classes/user';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/common/services/authentication.service';
+import { CommentsService } from 'src/app/common/services/comments.service';
+
 
 @Component({
   selector: 'app-komentarji-seznam',
@@ -7,16 +18,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KomentarjiSeznamComponent implements OnInit {
 
-  public novKomentar = {
-    naziv: '',
-    ocena: 5,
-    komentar: ''
+  public newComment = {
+    _id: '',
+    avtor: '',
+    ocena: null,
+    datum: '',
+    besediloKomentarja: '',
+    emailUporabnika: ''
   };
   
-  public obrazecPrikazan: boolean = false;
-  
-  constructor() { }
+  constructor(
+    private commentsService: CommentsService,
+    private instructionsService: InstructionsService,
+    private route: ActivatedRoute,
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) { }
 
+  ngOnInit(): void {
+    this.getEventInfo();
+  }
+
+  public obrazecPrikazan: boolean = false;
+
+  public sporocilo: string = "";
+
+  private dogodek;
+
+  public gostitelj: User;
+
+  public isAdmin: boolean = false;
+
+  private async getEventInfo(): Promise<void> {
+    this.route.paramMap
+      .pipe(
+        switchMap((params: ParamMap) => {
+          let eventID = params.get('idDogodka');
+          return this.instructionsService.getEventInfo(eventID);
+        })
+      )
+      .subscribe(async (event: InstructionsEvent) => {
+        this.dogodek = event;
+        if (this.authenticationService.isLoggedIn()) {
+          if (this.dogodek.emailInstruktorja === this.authenticationService.getCurrentUser().email) {
+            this.isAdmin = true;
+          }
+        }
+        this.sporocilo = event ? "" : "Dogodek ne obstaja :("
+        event ? this.getEventHost() : null;
+
+      });
+  }
+
+  private getEventHost(): void {
+    this.instructionsService.getEventHost(this.dogodek.emailInstruktorja)
+      .then(host => {
+        this.gostitelj = host;
+        this.sporocilo = host ? "" : "Ne najdem gostitelja dogodka :("
+      })
+      .catch(error => {
+        this.sporocilo = "Napaka API-ja pri iskanju gostitelja dogodka."
+        console.error(error);
+      });
+  }
+
+  postNewComment(): void {
+    this.newComment.emailUporabnika = this.authenticationService.getCurrentUser().email;
+
+    if(!this.newComment.avtor || 
+      !this.newComment.ocena || 
+      !this.newComment.besediloKomentarja) {
+      this.sporocilo = "Prosimo izpolnite vsa polja!"
+    } else {
+      this.commentsService.postNewComment(this.dogodek,this.newComment)
+      .then(comment => {
+        this.newComment = comment;
+        this.sporocilo = comment ? "Komentar uspešno objavljen :)" : "Napaka pri objavi komentarja :("
+        this.router.navigateByUrl('/instrukcije-dogodki/dogodek/${event._id}' + this.newComment._id);
+        alert("Komentar uspešno objavljen!");
+      })
+      .catch(error => {
+        this.sporocilo = "Napaka API-ja pri objavi dogodka."
+        //console.error(error);
+      });
+    } 
+  }
+
+  /* Testni podatki */
+  
   komentarji: Komentar[] = [{
     _id: "5fea45311d48a83b6b8dd016",
     avtor: "Anton Karlič",
@@ -38,9 +127,6 @@ export class KomentarjiSeznamComponent implements OnInit {
     besediloKomentarja: "Zanimivo in originalno, a ponekod pomanjkljivo.",
     datum: "30.12.2020"
   }]
-
-  ngOnInit(): void {
-  }
 
 }
 
